68f7f4f87796b242d868512446fe93e5cc89f530
Olatokunbo <Olatokunbo@Javas-MacBook-Pro.local>
2019-11-09 05:51:20 +0100

    Made header component dynamic by passing state

--- src/components/header.js
+++ src/components/header.js
@@ -2,16 +2,40 @@ import React from 'react'
 import Link from 'gatsby-link';
 import './header.css'
 
-const Header = ({ siteTitle }) => (
- <div className="Header">
-   <div className="HeaderGroup">
-   <Link to="/"><img src="https://p90.f3.n0.cdn.getcloudapp.com/items/rRunmy1v/tcmlo.png?v=63ddbfc440a54c9717b20db7a0370b3e" width="200"/></Link>
-   <Link to="/home">Home</Link>
-   <Link to="/about">About</Link>
-   <Link to="/blog">Blog</Link>
-   <Link to="/contact">Contact</Link>
- </div>
- </div>
-)
+class Header extends React.Component {
+  constructor(props) {
+    super(props)
 
+    this.state = {
+      hasScrolled: false
+    }
+  }
+
+  componentDidMount(){
+    window.addEventListener('scroll',this.handleScroll)
+  }
+
+  handleScroll = (event) => {
+    const scrollTop = window.pageYOffset
+    if (scrollTop > 50) {
+      this.setState({hasScrolled: true })
+    } else {
+      this.setState({ hasScrolled: false})
+    }
+  }
+
+  render() {
+    return (
+      <div className={this.state.hasScrolled ? 'Header HeaderScrolled' : 'Header'}>
+        <div className="HeaderGroup">
+          <Link to="/"><img src="https://p90.f3.n0.cdn.getcloudapp.com/items/rRunmy1v/tcmlo.png?v=63ddbfc440a54c9717b20db7a0370b3e" width="200" /></Link>
+          <Link to="/home">Home</Link>
+          <Link to="/about">About</Link>
+          <Link to="/blog">Blog</Link>
+          <Link to="/contact">Contact</Link>
+        </div>
+      </div>
+    )
+  }
+}
 export default Header
