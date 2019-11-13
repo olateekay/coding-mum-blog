2cfd0a441bffd8dfe9fbb3bda8a542fdf23484ea
Olatokunbo <Olatokunbo@Javas-MacBook-Pro.local>
2019-11-13 01:00:35 +0100

    added seo

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,8 @@
 module.exports = {
   siteMetadata: {
     title: 'The Coding Mum',
+    description: 'A mothers journey to becoming a software developer.', 
+    keywords:'mothers in tech, women in tech, react,coding mum,coding mom'
   },
   plugins: ['gatsby-plugin-react-helmet'],
 }
